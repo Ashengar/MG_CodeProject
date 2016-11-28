e7ddca9ccb4231a38b1dac188228fd6c757c3dd9
Ashengar <sdmarett@gmail.com>
2016-11-28 12:42:07 +0000

    MG_Setting Up Source Control and Unreal Editor

---
+++ MG_CodeProject/Source/MG_CodeProject/MG_CodeProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MG_CodeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MG_CODEPROJECT_API AMG_CodeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
