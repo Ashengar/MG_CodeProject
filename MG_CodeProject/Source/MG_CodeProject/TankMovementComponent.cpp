5852cc2bb41512486b0d58c53b125674c3b8fe08
Ashengar <sdmarett@gmail.com>
2016-12-02 09:24:40 +0000

    MG_Finishing Off Control Method C++/Blueprint

--- MG_CodeProject/Source/MG_CodeProject/TankMovementComponent.cpp
+++ MG_CodeProject/Source/MG_CodeProject/TankMovementComponent.cpp
@@ -1,12 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MG_CodeProject.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend Move Forward Throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
+
+
+
+//auto Time = GetWorld()->GetTimeSeconds();
+//UE_LOG(LogTemp, Warning, TEXT("Intend Move Forward Throw: %f"), Throw);
