71db52e645a9bf504a9c4319403c2006ead342a9
Ashengar <sdmarett@gmail.com>
2016-12-07 12:56:59 +0000

    MG_OnHit Event

--- MG_CodeProject/Source/MG_CodeProject/TankTrack.cpp
+++ MG_CodeProject/Source/MG_CodeProject/TankTrack.cpp
@@ -5,6 +5,36 @@
 
 
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm Hit"))
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	// Slippage
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Required Acceration
+	auto CorrectionAcceration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+	// Apply sideways Friction
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
