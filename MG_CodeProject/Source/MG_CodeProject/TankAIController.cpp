9a574f51849ff2917819a23c383e5026ca4ce7f6
Ashengar <sdmarett@gmail.com>
2016-12-08 09:22:23 +0000

    MG_Tweaking Tank AI

--- MG_CodeProject/Source/MG_CodeProject/TankAIController.cpp
+++ MG_CodeProject/Source/MG_CodeProject/TankAIController.cpp
@@ -31,8 +31,11 @@ void ATankAIController::Tick(float DeltaTime)
 		auto AimingComponent = ControlledTank->FindComponentByClass<UTankAimingComponent>();
 		AimingComponent->AimAt(PlayerTank->GetActorLocation());
 		
-		AimingComponent->Fire();
-	
+		// if aiming or locked fire
+		if (AimingComponent->GetFiringState() == EFiringState::Locked)
+		{
+			AimingComponent->Fire();
+		}
 
 }
 
--- MG_CodeProject/Source/MG_CodeProject/TankAIController.h
+++ MG_CodeProject/Source/MG_CodeProject/TankAIController.h
@@ -14,12 +14,16 @@ UCLASS()
 class MG_CODEPROJECT_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
+
+protected:
+	// How close AI tank can get to player
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float AcceptanceRadius = 8000;
 	
 private:	
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaSeconds) override;
 
-	float AcceptanceRadius = 3000;
-
+	
 };
